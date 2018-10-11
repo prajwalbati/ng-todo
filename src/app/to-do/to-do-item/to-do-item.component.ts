8921337e4dd42d904dbe53ed016adbd79e1bcbaa
Prajwal Bati <prajwalbati@gmail.com>
2018-10-11 12:52:17 +0545

    Changes

--- src/app/to-do/to-do-item/to-do-item.component.ts
+++ src/app/to-do/to-do-item/to-do-item.component.ts
@@ -2,20 +2,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { ToDo } from "../to-do";
 
 @Component({
-	selector: "ToDoItem", //default: app-to-do-item
-	templateUrl: "./to-do-item.component.html",
-	styleUrls: ["./to-do-item.component.css"]
+    selector: "ToDoItem",
+    templateUrl: "./to-do-item.component.html",
+    styleUrls: ["./to-do-item.component.css"]
 })
 export class ToDoItemComponent implements OnInit {
-	constructor() {}
+    constructor() {}
 
-	@Input() ToDo: ToDo = new ToDo("");
+    @Input() ToDo: ToDo = new ToDo("");
 
-	@Output() Deleted: EventEmitter<any> = new EventEmitter();
+    @Output() Deleted: EventEmitter<any> = new EventEmitter();
 
-	ngOnInit() {}
+    ngOnInit() {}
 
-	deleteItem() {
-		this.Deleted.emit();
-	}
+    deleteItem() {
+        this.Deleted.emit();
+    }
 }
--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -2,43 +2,43 @@ import { Component, OnInit } from "@angular/core";
 import { ToDo } from "./to-do";
 
 @Component({
-	selector: "ToDo", // default:'app-to-do',
-	templateUrl: "./to-do.component.html",
-	styleUrls: ["./to-do.component.css"]
+    selector: "ToDo", // default:'app-to-do',
+    templateUrl: "./to-do.component.html",
+    styleUrls: ["./to-do.component.css"]
 })
 export class ToDoComponent implements OnInit {
-	constructor() {}
+    constructor() {}
 
-	ToDos: ToDo[] = [new ToDo("clean the house"), new ToDo("buy milk")];
+    ToDos: ToDo[] = [new ToDo("clean the house"), new ToDo("buy milk")];
 
-	Input: string = "";
+    Input: string = "";
 
-	Submitted: boolean = false;
+    Submitted: boolean = false;
 
-	ngOnInit() {}
+    ngOnInit() {}
 
-	deleteItem(i: number) {
-		//note delete doesn't work: https://stackoverflow.com/a/40462431/2653503
-		this.ToDos.splice(i, 1);
-	}
+    deleteItem(i: number) {
+        //note delete doesn't work: https://stackoverflow.com/a/40462431/2653503
+        this.ToDos.splice(i, 1);
+    }
 
-	addItem() {
-		this.Submitted = true;
-		if (this.Input == "") {
-			return;
-		}
-		this.ToDos = this.ToDos.concat(new ToDo(this.Input));
-	}
+    addItem() {
+        this.Submitted = true;
+        if (this.Input == "") {
+            return;
+        }
+        this.ToDos = this.ToDos.concat(new ToDo(this.Input));
+    }
 
-	ShouldBeHidden(
-		Valid: boolean,
-		Pristine: boolean,
-		Submitted: boolean
-	): boolean {
-		if (Submitted) {
-			return Valid;
-		} else {
-			return Valid || Pristine;
-		}
-	}
+    ShouldBeHidden(
+        Valid: boolean,
+        Pristine: boolean,
+        Submitted: boolean
+    ): boolean {
+        if (Submitted) {
+            return Valid;
+        } else {
+            return Valid || Pristine;
+        }
+    }
 }
