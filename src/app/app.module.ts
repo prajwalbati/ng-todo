879b58aa52a37e1e89bc894d4ed2e465cf540752
Prajwal Bati <prajwal.bati@ekbana.info>
2019-11-26 11:09:23 +0545

    Update project with login using firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+
+// Firebase services + enviorment module
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+import { AuthService } from "./shared/services/auth.service";
+
 import { AppComponent } from "./app.component";
 import { ToDoComponent } from "./to-do/to-do.component";
 import { ToDoItemComponent } from "./to-do/to-do-item/to-do-item.component";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 @NgModule({
-  declarations: [AppComponent, ToDoComponent, ToDoItemComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
+  declarations: [AppComponent, ToDoComponent, ToDoItemComponent, DashboardComponent, SignInComponent, SignUpComponent, ForgotPasswordComponent, VerifyEmailComponent],
+  imports: [AppRoutingModule, BrowserModule, FormsModule, AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/shared/guard/secure-inner-pages.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from "../../shared/services/auth.service";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SecureInnerPagesGuard implements CanActivate {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if(this.authService.isLoggedIn) {
+      console.log("You are not allowed to access this URL!");
+       this.router.navigate(['/dashboard']);
+       return false;
+    } else {
+      return true;
+    }
+    // return true;
+  }
+
+}
